e1fffba6519ed486ae49d26615166290a5f7d1c9
xinyisherryz <zhang.xinyi15@northeastern.edu>
2024-04-18 12:48:31 -0700

    check library version

---
+++ version.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib as plt
+import joblib
+
+print("Streamlit version:", st.__version__)
+print("Pandas version:", pd.__version__)
+print("Seaborn version:", sns.__version__)
+print("Matplotlib version:", plt.__version__)
+print("Joblib version:", joblib.__version__)
