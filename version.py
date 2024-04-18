b24cdb674b98a523afbfa1cd2bd1660239eb5070
xinyisherryz <zhang.xinyi15@northeastern.edu>
2024-04-18 12:54:15 -0700

    add sklearn version

--- version.py
+++ version.py
@@ -3,9 +3,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib as plt
 import joblib
+from sklearn import __version__ as skl_version
 
 print("Streamlit version:", st.__version__)
 print("Pandas version:", pd.__version__)
 print("Seaborn version:", sns.__version__)
 print("Matplotlib version:", plt.__version__)
 print("Joblib version:", joblib.__version__)
+print("Scikit-learn version:", skl_version)
